ae50cbc1afcf9fcf63f2864f2d7341702c1a7931
Serghei Iakovlev <git@serghei.pl>
2025-06-07 13:39:01 +0200

    Add base model classes

---
+++ cityhive/models.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timezone
+
+import sqlalchemy as sa
+from sqlalchemy import orm as so
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import func
+
+
+class RecordNotFound(Exception):
+    """Requested record in database was not found"""
+
+
+class Base(so.DeclarativeBase):
+    """Base class for all models."""
+
+    @classmethod
+    async def get_or_fail(cls, session: AsyncSession, entity_id: int):
+        """Return the model with the specified identifier.
+
+        Args:
+            session: The database session to use.
+            entity_id: The identifier of the model to return.
+
+        Returns:
+            The model with the specified identifier.
+
+        Raises:
+            RecordNotFound: If the model with the specified identifier does not exist.
+        """
+        result = await session.get(cls, entity_id)
+        if not result:
+            raise RecordNotFound(f"{cls.__name__} with id: {entity_id} does not exist")
+        return result
+
+
+class IdentityMixin:
+    """Mixin class to add identity fields to a SQLAlchemy model."""
+
+    id: so.Mapped[int] = so.mapped_column(
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    def __repr__(self):
+        """Returns the object representation in string format."""
+        return f"<{self.__class__.__name__} id={self.id!r}>"
+
+
+class TimestampMixin:
+    """Mixin class to add timestamp fields to a SQLAlchemy model."""
+
+    created_at: so.Mapped[datetime] = so.mapped_column(
+        sa.DateTime(timezone=True),
+        nullable=False,
+        index=True,
+        server_default=func.now(),
+    )
+    updated_at: so.Mapped[datetime] = so.mapped_column(
+        sa.DateTime(timezone=True),
+        nullable=False,
+        index=True,
+        onupdate=func.now(),
+        server_default=func.now(),
+    )
+
+    def last_modified(self) -> str:
+        """Get the time of the last model update."""
+        modified = self.updated_at or self.created_at or datetime.now(timezone.utc)
+        return modified.strftime("%a, %d %b %Y %H:%M:%S GMT")
